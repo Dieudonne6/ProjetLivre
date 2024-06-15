6dd3e1071797aae85d6cf04093f24efc38505d07
@Franck229 <dieudonneayena6@gmail.com>
2024-06-15 18:56:02 +0100

    authentification

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
 
 class AuthController extends Controller
 {
@@ -14,7 +16,11 @@ class AuthController extends Controller
         $validations = Validator::make($request->all(), [
             'name' => 'required|string',
             'email' => 'required|email|unique:users|max:155',
-            'password' => 'required|min:6'
+            // 'image' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'password' => 'required|min:6',
+            'telephone' => 'required|max:8',
+            'statut' => 'required'
         ]);
 
 
@@ -28,17 +34,65 @@ class AuthController extends Controller
         }
 
         if ($validations->passes()) {
+            // $imageName = "kokoko";
+            $imageName = Str::random(32).".".$request->image->getClientOriginalExtension();
+            if(($request->statut) === 1){
+                $user = User::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'image' => $imageName,
+                    'password' => Hash::make($request->password),
+                    'telephone' => $request->telephone,
+                    'statut' => 1,
+                ]);
+    
+                Storage::disk('public')->put($imageName, file_get_contents($request->image));
+                $token = $user->createToken('auth_token')->plainTextToken;
+                return response()-> json([
+                    'token' => $token,
+                    'type' => 'Bearer',
+                    'msg' => 'inscription effectuer avec succes'
+                ]);
+            }
+
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
+                'image' => $imageName,
                 'password' => Hash::make($request->password),
+                'telephone' => $request->telephone,
+                'statut' => 0,
             ]);
 
+            Storage::disk('public')->put($imageName, file_get_contents($request->image));
             $token = $user->createToken('auth_token')->plainTextToken;
             return response()-> json([
                 'token' => $token,
-                'type' => 'Bearer'
+                'type' => 'Bearer',
+                'msg' => 'inscription effectuer avec succes'
             ]);
+           
         }
     }
+
+
+    public function login(Request $request) {
+        if(!Auth::attempt($request->only('email', 'password'))) {
+            return response() -> json([
+                'msg' => 'email ou mot de passe incorrect',
+                'statut' => 401
+            ]);
+        }
+
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()-> json([
+            'token' => $token,
+            'type' => 'Bearer'
+        ])->cookie('jwt', $token);
+
+    }
+
+
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
